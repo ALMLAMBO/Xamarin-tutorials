46a9d2448aa5361638bca6e64a143b44f760aac2
ALMLAMBO <aleksm007148@gmail.com>
2020-03-07 16:17:15 +0200

    Add project files.

---
+++ SimpleListView/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace SimpleListView {
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity {
+        private List<string> items;
+        private AbsListView myListView;
+
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            items = new List<string>() {
+                "Pesho",
+                "Gosho",
+                "Tosho"
+            };
+
+            myListView = FindViewById<AbsListView>(Resource.Id.myListView);
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>
+                (this,
+                Android.Resource.Layout.SimpleListItem1, items);
+
+            myListView.Adapter = adapter;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ SimpleListView/MyListViewAdapter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SimpleListView {
+	public class MyListViewAdapter : BaseAdapter<string> {
+		private List<string> items;
+		private Context context;
+
+		public MyListViewAdapter(Context context, List<string> items) {
+			this.items = items;
+			this.context = context;
+		}
+
+		public override string this[int position] => items[position];
+
+		public override int Count { get => items.Count; }
+
+		public override long GetItemId(int position) {
+			return position;
+		}
+
+		public override View GetView(int position, View convertView, ViewGroup parent) {
+			throw new NotImplementedException();
+		}
+	}
+}
